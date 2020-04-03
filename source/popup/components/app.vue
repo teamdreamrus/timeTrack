c044b9aa6af4786afd27b84ff176cf8d899212a3
teamdreamrus <atazhanov.biz@gmail.com>
2020-04-04 00:15:49 +0700

    отрисовка статикой, для динамики нет данных к моменту отрисовки

--- source/popup/components/app.vue
+++ source/popup/components/app.vue
@@ -1,20 +1,31 @@
 <template>
     <div>
         <div v-if="data!==[]">
-            <ul>
-                <li v-for="item in data">
-                    {{ item.count }} - {{item.hostname}}
-                </li>
-            </ul>
             <button @click="refresh">refresh</button>
+
+            <div v-for="(item, index) in data">
+                <div class="" v-if="index < 5">
+                    {{ item.count }} - {{item.hostname}}
+                </div>
+            </div>
+
+            <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
         </div>
+<!--        <div class="small">-->
+<!--            <line-chart :chart-data="datacollection"></line-chart>-->
+<!--                        <button @click="fillData()">Randomize</button>-->
+<!--        </div>-->
     </div>
+
 
 </template>
 <script>
     import Vue from 'vue';
     import AsyncComputed from 'vue-async-computed'
     import {getStorageData} from '../../utils'
+    import PieChart from './PieChart.js'
+    // import PieChart from "./PieChart";
+    import {Bar} from 'vue-chartjs'
 
     Vue.use(AsyncComputed);
 
@@ -23,7 +34,22 @@
         data() {
             return {
                 data: [],
-              timesPostHasBeenUpdated: 0
+                timesPostHasBeenUpdated: 0,
+                chartOptions: {
+                    hoverBorderWidth: 20
+                },
+                chartData: {
+                    hoverBackgroundColor: "red",
+                    hoverBorderWidth: 10,
+                    labels: ["Green", "Red", "Blue", 'white', 'yellow'],
+                    datasets: [
+                        {
+                            label: "Data One",
+                            backgroundColor: this.getRandomColors(),
+                            data: [1, 10, 5,5,3]
+                        }
+                    ]
+                }
             };
         },
         methods: {
@@ -33,12 +59,49 @@
                 this.data.push(...result.data);
                 console.log(this.data);
             },
-            beforeMount(){
-                this.refresh();
+            getRandomColors() {
+                let colors = [];
+                for (let i = 0; i < 5; i++) {
+                    colors.push("#"+((1<<24)*Math.random()|0).toString(16))
+                }
+                return colors;
             },
+            getFirstFiveHostnames() {
+                return this.data.slice(0, 5).map(el => {
+                    return el.hostname;
+                });
+            },
+            getFirstFiveCounts() {
+                return this.data.slice(0, 5).map(el => {
+                    return el.count;
+                });
+            }
+            // fillData() {
+            //     this.datacollection = {
+            //         labels: [this.getRandomInt(), this.getRandomInt()],
+            //         datasets: [
+            //             {
+            //             label: "Data One",
+            //             backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            //             data: [1, 10, 5]
+            //         }
+            //         ]
+            //     }
+            // },
+            // getRandomInt() {
+            //     return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+            // }
+        },
+        beforeMount() {
+            this.refresh();
+        },
+        mounted() {
+            // this.fillData();
         },
         watch: {},
-        components: {},
+        components: {
+            pieChart: PieChart
+        },
         asyncComputed: {
             // getData: {
             //     get() {
