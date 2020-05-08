acb35b7199374cfe562506f41716bda941473ad4
teamdreamrus <atazhanov.biz@gmail.com>
2020-05-09 00:59:52 +0700

    workMode toggle-button

--- source/options/components/Workmode.vue
+++ source/options/components/Workmode.vue
@@ -1,11 +1,31 @@
 <template>
-  <h1>Work Mode</h1>
+  <div class="d-flex justify-content-center align-items-center flex-column">
+    <h5>Work-mode</h5>
+    <div class="d-flex justify-content-center flex-row">
+      <div class="mr-2" :class="!status ? 'fontBold' : ''">off</div>
+      <b-form-checkbox size="lg" v-model="status" name="check-button" switch> </b-form-checkbox>
+      <div :class="status ? 'fontBold' : ''">on</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Workmode',
+  data() {
+    return {
+      status: false,
+    };
+  },
+  methods: {
+    //save toggle to storage
+  },
+  beforeMount() {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.fontBold {
+  font-weight: bold;
+}
+</style>
